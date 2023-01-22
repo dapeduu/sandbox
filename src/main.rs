c4c3392316dfadbc2e4051f5c2b6284d018db57f
Anon1929 <viniciuslimapassos@gmail.com>
2023-01-22 14:40:49 -0300

    Encapsulamento completo, troca de particulas

--- src/main.rs
+++ src/main.rs
@@ -16,7 +16,7 @@ use crate::implparticles::*;
 fn main() -> Result<(), Error> {
     env_logger::init();
     let mut clickflag: bool = true;
-    let mut particlekey: i32 =0;
+    let mut particlekey: ParticleNum =ParticleNum::Sand;
     let event_loop = EventLoop::new();
     let mut input = WinitInputHelper::new();
     let window = {
@@ -34,43 +34,9 @@ fn main() -> Result<(), Error> {
         let surface_texture = SurfaceTexture::new(window_size.width, window_size.height, &window);
         Pixels::new(WIDTH, HEIGHT, surface_texture)?
     };
-    //pixels.set_clear_color([0,0,]);
 
-    let mut particlevec: Vec<SandParticle> = Vec::new();
+    let mut particlevec: Vec<ParticleType> = Vec::new();
 
-    let mut particlevec2: Vec<ParticleType> = Vec::new();
-
-    // Teste vec de enums
-    /* 
-    let parteste1 = SandParticle {
-        x: 1 as u32,
-        y: 2 as u32,
-        rgba: [0xef, 0xef, 0x00, 0xff],
-    };
-    let parteste2 = Particle {
-        x: 3 as u32,
-        y: 4 as u32,
-        rgba: [0xef, 0xef, 0x00, 0xff],
-    };
-    
-    particlevec2.push(ParticleType::SandParticle(parteste1));
-    particlevec2.push(ParticleType::Particle(parteste2));
-
-    for partenum in particlevec2{
-        match partenum{
-            ParticleType::SandParticle(part)=> {
-                println!("{}", part.x);
-                        }
-            ParticleType::Particle(part) => {
-                println!("{}", part.x);
-            }
-        }
-    }
-
-    process::exit(1);
-
-    */
-    // Teste vec de enums
 
     event_loop.run(move |event, _, control_flow| {
         println!("Number of particles: {}", particlevec.len());
@@ -78,11 +44,7 @@ fn main() -> Result<(), Error> {
         // Draw the current frame
         if let Event::RedrawRequested(_) = event {
             draw(pixels.get_frame_mut(), particlevec.clone());
-            if pixels
-                .render()
-                .map_err(|e| error!("pixels.render() failed: {}", e))
-                .is_err()
-            {
+            if pixels.render().map_err(|e| error!("pixels.render() failed: {}", e)).is_err(){
                 *control_flow = ControlFlow::Exit;
                 return;
             }
@@ -99,67 +61,109 @@ fn main() -> Result<(), Error> {
             if input.key_pressed(VirtualKeyCode::P){
                 clickflag = !clickflag;
             }
+            if input.key_pressed(VirtualKeyCode::Key1){
+                particlekey = ParticleNum::Base;
+            }
+            if input.key_pressed(VirtualKeyCode::Key2){
+                particlekey = ParticleNum::Sand;
+            }
+
             if clickflag {
                 if input.mouse_held(0) {
-        
-                    let mousepos = input.mouse().unwrap();
-                    let pixelpos = pixels
-                    .window_pos_to_pixel(mousepos)
-                    .unwrap_or_else(|pos| pixels.clamp_pixel_pos(pos));
-                    let index: usize = position_to_index(pixelpos.0 as u32, pixelpos.1 as u32);
-                    let frame:&mut[u8] = pixels.get_frame_mut();
-                    if (frame[index] == 150){
-                        let novaparticula = SandParticle {
-                            x: pixelpos.0 as u32,
-                            y: pixelpos.1 as u32,
-                            rgba: [0xef, 0xef, 0x00, 0xff],
-                        };
-                        particlevec.push(novaparticula);
+                    let pixref = &mut pixels as *mut Pixels;
+                    match instanceparticle(&input,pixref,particlekey){
+                        Some(instancia)=>{ particlevec.push(instancia); }
+                        None => {}
                     }
                 }
             }
             else{
                 if input.mouse_pressed(0) {
-                    let mousepos = input.mouse().unwrap();
-                    let pixelpos = pixels
-                    .window_pos_to_pixel(mousepos)
-                    .unwrap_or_else(|pos| pixels.clamp_pixel_pos(pos));
-                    let index: usize = position_to_index(pixelpos.0 as u32, pixelpos.1 as u32);
-                    let frame:&mut[u8] = pixels.get_frame_mut();
-                    if (frame[index] == 150){
-                        let novaparticula = SandParticle {
-                            x: pixelpos.0 as u32,
-                            y: pixelpos.1 as u32,
-                            rgba: [0xef, 0xef, 0x00, 0xff],
-                        };
-                        particlevec.push(novaparticula);
-                    }
+                    let pixref = &mut pixels as *mut Pixels;
+                    match instanceparticle(&input,pixref,particlekey){
+                        Some(instancia)=>{ particlevec.push(instancia); }
+                        None => {}
+                    }  
                 }
-  
-        }    
+            }    
             if let Some(size) = input.window_resized() {
                 pixels.resize_surface(size.width, size.height).unwrap();
             }
 
             update(particlevec.as_mut_slice(), pixels.get_frame_mut());
+
             window.request_redraw();
         }
     });
 }
-pub fn update(vec: &mut [SandParticle], frame: &mut [u8]) {
-        for particle in vec {
-            particle.move_particle(frame);
+
+pub fn instanceparticle(input: *const WinitInputHelper,pixels:*mut Pixels, particlekey: ParticleNum)->Option<ParticleType>{
+    unsafe{
+    let mousepos = (*input).mouse().unwrap(); 
+    let pixelpos = (*pixels).window_pos_to_pixel(mousepos).unwrap_or_else(|pos| (*pixels).clamp_pixel_pos(pos));
+    let index: usize = position_to_index(pixelpos.0 as u32, pixelpos.1 as u32);
+    let frame:&mut[u8] = (*pixels).get_frame_mut();
+    if (frame[index] == 150){
+        match particlekey{
+            ParticleNum::Base=>{
+                let novaparticula = Particle {
+                    x: pixelpos.0 as u32,
+                    y: pixelpos.1 as u32,
+                    rgba: [0x00, 0xef, 0x00, 0xff],
+                };
+                return Some((ParticleType::Particle(novaparticula)));
+            }
+            ParticleNum::Sand=>{
+                let novaparticula = SandParticle {
+                    x: pixelpos.0 as u32,
+                    y: pixelpos.1 as u32,
+                    rgba: [0xef, 0xef, 0x00, 0xff],
+                };
+                return Some((ParticleType::SandParticle(novaparticula)));
+            }
         }
     }
+    else{
+        return None;
+    }
+}
+}
+pub fn update(vec: &mut [ParticleType], frame: &mut [u8]) {
+    for partenum in vec{
+        match partenum{
+            ParticleType::SandParticle(part)=> {
+                part.move_particle(frame);
+            }
+            ParticleType::Particle(part) => {
+                part.move_particle(frame);
+            }
+        }
+    }
+}
 
-pub fn draw(frame: &mut [u8], vec: Vec<SandParticle>) {
+pub fn draw(frame: &mut [u8], vec: Vec<ParticleType>) {
         //clear(frame);
         frame.fill(150);
-        for particle in &vec {
-            let index: usize = position_to_index(particle.x, particle.y);
-            frame[index] = particle.rgba[0]; //r
-            frame[index + 1] = particle.rgba[1]; //g    //[][][][][] WIDTH*Heigh /30000  0   1    2    3      --- 400
-            frame[index + 2] = particle.rgba[2]; //b    //                               400 401 402 403          400
-            frame[index + 3] = particle.rgba[3]; //a    //                               800 801 803 803 -
+
+        for partenum in vec{
+            match partenum{
+                ParticleType::SandParticle(part)=> {
+                    let index: usize = position_to_index(part.x, part.y);
+                    frame[index] = part.rgba[0];     //r
+                    frame[index + 1] = part.rgba[1]; //g    
+                    frame[index + 2] = part.rgba[2]; //b    
+                    frame[index + 3] = part.rgba[3]; //a   
+                }
+                ParticleType::Particle(part) => {
+                    let index: usize = position_to_index(part.x, part.y);
+                    frame[index] = part.rgba[0];     //r
+                    frame[index + 1] = part.rgba[1]; //g    
+                    frame[index + 2] = part.rgba[2]; //b    
+                    frame[index + 3] = part.rgba[3]; //a   
+                }
+            }
         }
+        //[][][][][] WIDTH*Heigh /30000  0   1    2    3      --- 400
+        //                               400 401 402 403          400
+        //                               800 801 803 803 -
     }
