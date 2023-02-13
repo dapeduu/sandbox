511221a59b09607f673ad1d5ecbce19658ddb9c7
EduardoAfInacio <eduafinacio@gmail.com>
2023-02-13 20:50:25 -0300

    Interação entre água e ácido adicionada.

--- src/implparticles.rs
+++ src/implparticles.rs
@@ -81,52 +81,7 @@ impl BaseParticle for AcidParticle {
     //Mover para baixo se possível.
     //Mover para baixo e depois para esquerda ou direita aleatoriamente.
     //Não ser possível mais descer, logo, mover para a esquerda ou direita aleatoriamente.
-
-    //Captar quais particulas o ácido pode dissolver.
-    // fn acid_dissolves(&self) -> bool {
-    // }
-
-    //Atualmente tem o mesmo comportamento da areia, mas apenas pra não dar ruim na hora de rodar o cargo run. Comportamento ainda será implementado.
-    fn move_particle(&mut self, frame: &mut [u8]) {
-        if self.colision(frame) {
-            return;
-        }
-        let mut index: usize = position_to_index(self.x, self.y + 1);
-        if (frame[index + 2]) != 150 {
-            if self.x != 0 {
-                index = position_to_index(self.x - 1, self.y + 1);
-                if frame[index + 2] == 150 {
-                    self.y += 1;
-                    self.x -= 1;
-                    return;
-                }
-            }
-            if self.x != WIDTH - 1 {
-                index = position_to_index(self.x + 1, self.y + 1);
-                if frame[index + 2] == 150 {
-                    self.y += 1;
-                    self.x += 1;
-                    return;
-                }
-            }
-        } else {
-            self.y += 1;
-            return;
-        }
-    }
-    fn colision(&self, _: &mut [u8]) -> bool {
-        if self.y + 1 >= HEIGHT {
-            return true;
-        }
-        return false;
-    }
-}
-
-impl BaseParticle for WaterParticle {
-    // Move para baixo se possível
-    // Caso contrário, move aleatoriamente para esquerda ou direita
-    // Objetivo: preencher todos os espaços do nível inferior
-
+    //Comportamento parecido com da água.
     fn move_particle(&mut self, frame: &mut [u8]) {
         if self.colision(frame) {
             return;
@@ -167,8 +122,85 @@ impl BaseParticle for WaterParticle {
     }
 }
 
+impl BaseParticle for WaterParticle {
+    //Move para baixo se possível
+    //Caso contrário, move aleatoriamente para esquerda ou direita
+    //Objetivo: preencher todos os espaços do nível inferior
+    //Uma boa quantidade de água "dissolve" o ácido, sobrescrevendo-o.
+    fn move_particle(&mut self, frame: &mut [u8]) {
+        if self.colision(frame) {
+            return;
+        }
+
+        fn dissolves_element(frame: &mut [u8], index: usize) -> bool {
+            let is_on_acid = frame[index] == 0x0 // 
+                && frame[index + 1] == 0x80 // G 
+                && frame[index + 2] == 0x0 // B 
+                && frame[index + 3] == 0xff; // A 
+
+            return is_on_acid;
+        }
+
+        let index_down = position_to_index(self.x, self.y + 1);
+        if dissolves_element(frame, index_down){
+            self.y += 1;
+            frame[index_down] = 0x00; // R
+            frame[index_down + 1] = 0x00; // G
+            frame[index_down + 2] = 0xff; // B
+            frame[index_down + 3] = 0xff; // A
+        }
+        if frame[index_down + 2] == 150 {
+            self.y += 1;
+        }else {
+            let mut new_x = self.x;
+            let mut new_y = self.y;
+            let direction = rand::thread_rng().gen_range(0, 2);
+            if self.x > 0 && direction == 0 {
+                let index_left = position_to_index(self.x - 1, self.y);
+                if dissolves_element(frame, index_left){
+                    new_x = self.x - 1;
+                    new_y = self.y;
+                    frame[index_left] = 0x00; // R
+                    frame[index_left + 1] = 0x00; // G
+                    frame[index_left + 2] = 0xff; // B
+                    frame[index_left + 3] = 0xff; // A
+                }
+                if frame[index_left + 2] == 150 {
+                    new_x = self.x - 1;
+                    new_y = self.y;
+                }
+            }
+            if self.x < WIDTH - 1 && direction == 1 {
+                let index_right = position_to_index(self.x + 1, self.y);
+                if dissolves_element(frame, index_right){
+                    new_x = self.x + 1;
+                    new_y = self.y;
+                    frame[index_right] = 0x00; // R
+                    frame[index_right + 1] = 0x00; // G
+                    frame[index_right + 2] = 0xff; // B
+                    frame[index_right + 3] = 0xff; // A
+                }
+                if frame[index_right + 2] == 150 {
+                    new_x = self.x + 1;
+                    new_y = self.y;
+                }
+            }
+            if new_x != self.x || new_y != self.y {
+                self.x = new_x;
+                self.y = new_y;
+            }
+        }
+    }
+
+    fn colision(&self, _: &mut [u8]) -> bool {
+        if self.y + 1 >= HEIGHT {
+            return true;
+        }
+        return false;
+    }
+}
+
 impl BaseParticle for AgitatedParticle {
-    //Inspirada no comportamento da partícula de água.
     fn move_particle(&mut self, frame: &mut [u8]) {
         let direction = rand::thread_rng().gen_range(0, 4);
         let mut new_x = self.x;
@@ -272,3 +304,5 @@ impl BaseParticle for ElectricityParticle {
         return false;
     }
 }
+
+
